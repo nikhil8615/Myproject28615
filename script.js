fb9e9cb31e7306507dd1e2771068e01634e733b0
Nikhil Singh <124617876+nikhil8615@users.noreply.github.com>
2023-10-12 17:07:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-function firstPageAnim() {
-  var tl = gsap.timeline();
+// function firstPageAnim() {
+//   var tl = gsap.timeline();
 
-  tl.from("#nav", {
-    y: "-10",
-    opacity: 0,
-    duration: 1.5,
-    ease: Expo.easeInOut,
-  })
-    .to(".boundingelem", {
-      y: 0,
-      ease: Expo.easeInOut,
-      duration: 2,
-      delay: -1,
-      stagger: 0.2,
-    })
-    .from("#herofooter", {
-      y: -10,
-      opacity: 0,
-      duration: 1.5,
-      delay: -1,
-      ease: Expo.easeInOut,
-    });
-}
+//   tl.from("#nav", {
+//     y: "-10",
+//     opacity: 0,
+//     duration: 1.5,
+//     ease: Expo.easeInOut,
+//   })
+//     .to(".boundingelem", {
+//       y: 0,
+//       ease: Expo.easeInOut,
+//       duration: 2,
+//       delay: -1,
+//       stagger: 0.2,
+//     })
+//     .from("#herofooter", {
+//       y: -10,
+//       opacity: 0,
+//       duration: 1.5,
+//       delay: -1,
+//       ease: Expo.easeInOut,
+//     });
+// }
 
-function circleChaptaKaro() {
+function circleChapta() {
   // define default scale value
   var xscale = 1;
   var yscale = 1;
@@ -65,7 +65,7 @@ function circleMouseFollower(xscale, yscale) {
   });
 }
 
-circleChaptaKaro();
+circleChapta();
 circleMouseFollower();
 // firstPageAnim();
 
